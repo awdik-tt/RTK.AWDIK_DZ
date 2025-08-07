1590e94e5e36e00a6dadc0f7f3bb40f4d844d8ea
awdik-tt <awdik.tt@gmail.com>
2025-08-07 17:39:25 +0500

    изменения в ДЗ: Дополнено работой по ДЗ_4 (Телевизор)

--- DZ/src/Task3_Privet/Privet.java
+++ DZ/src/Task3_Privet/Privet.java
@@ -3,7 +3,7 @@ package Task3_Privet;
 import java.util.Scanner;
 
 public class Privet {
-        public static void main(String[] args) {
+        public static void main(String[] args) {    //объявляем публичный статичный метод
             Scanner scanner = new Scanner(System.in);
             System.out.print("Введите имя: ");
             String name = scanner.nextLine();
--- DZ/src/Task4_TV/TV.java
+++
@@ -1,4 +0,0 @@
-package Task4_TV;
-
-public class TV {
-}
